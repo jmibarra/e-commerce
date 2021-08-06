47852ab82d28397a133f410a2089a2f80178463f
Juan Ibarra <jmibarra86@gmail.com>
2021-08-06 19:43:59 -0300

    Se crean los componentes de producto, productos y navbar

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+import {Products, Navbar} from './components'
+
+
+const app = () => {
+    return (
+        <div>
+            <Navbar />
+            <Products />
+        </div>
+    )
+}
+
+export default app
---
+++ src/components/Products/Product/Product.jsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import {Card, CardMedia, CardContent, CardActions, Typography, IconButton } from '@material-ui/core';
+import { AddShoppingCart } from '@material-ui/icons';
+
+import productStyle from './productStyle';
+
+const Product = ({ product }) => {
+    
+    const classes = productStyle()
+    
+    return (
+        <Card className={classes.root}>
+            <CardMedia className={classes.media} image={product.image} title={product.name}/>
+            <CardContent>
+                <div className={classes.CardContent}>
+                    <Typography variant="h5" gutterBottom>
+                        {product.name}
+                    </Typography>
+                    <Typography variant="h5">
+                        {product.price}
+                    </Typography>
+                    <Typography variant="body2" color="textSecondary">
+                        {product.description}
+                    </Typography>
+                    <CardActions disableSpacing className={classes.cardActions}>
+                        <IconButton arial-label="Add to Cart">
+                            <AddShoppingCart/>
+                        </IconButton>
+
+                    </CardActions>
+
+                </div>
+            </CardContent>
+
+        </Card>
+    )
+}
+
+export default Product
